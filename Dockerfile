7cb0daee4265e7a06337db15f00e52b72ea79797
Daniela Baron <dbaron@tucows.com>
2020-05-30 17:47:26 -0400

    action implementation with git release manager

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,16 @@ LABEL version="0.1.0"
 LABEL repository="https://github.com/dbaron-tc/auto-release-milestone"
 LABEL maintainer="Daniela"
 
+# Install jq for json parsing at command line
+RUN apt-get update && apt-get install -y jq
+
+# Install GitReleaseManager globally it can be invoked from anywhere
+RUN dotnet tool install -g GitReleaseManager.Tool
+
+# Update PATH to include GitReleaseManager
+ENV PATH /root/.dotnet/tools:$PATH
+
 # Specify path to shell script to be run as soon as container starts up
+# args to entrypoint.sh are passed in via args property of action metadata: action.yml
 COPY entrypoint.sh /
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,56 @@
 # tell bash to warn anytime an undeclared variable is used in script
 set -u
 
-# logging command for github action to set an output parameter
-echo "::set-output name=release-url::http://example.com"
+# save value of repo token into a variable (its first param passed to script as per action metadata args)
+repo_token=$1
+
+# verify this action got trigerred by a milestone event
+# use of debug logging command means this will only show in build log when debug is enabled
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
+  echo "::debug::The event name was '$GITHUB_EVENT_NAME'"
+  exit 0
+fi
+
+# check event payload with jq, `action` is at top level of event json
+# json strings are surrounded by quotes, use raw-output option to get just value
+event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
+
+# check for closed milestone
+if [ $event_type != "closed" ]; then
+  echo "::debug::The event type was '$event_type'"
+  exit 0
+fi
+
+# get the closed milestone's name
+milestone_name=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
+
+# Github action exports env var $GITHUB_REPOSITORY which contains: owner/repository
+# Parse this info for gitreleasemanager using bash tool IFS (internal field separator)
+# <<< is Here-string Redirection Operator - send string content to standard input stream of a command line program
+# in this case, value of $GITHUB_REPOSITORY is sent to read command
+IFS='/' read owner repository <<< "$GITHUB_REPOSITORY"
+
+# determine the release_url by invoking gitreleasemanager with some arguments:
+# 1. milestone from which to extract the closed issues (this becomes the release notes)
+# 2. tag to be associated with release - $GITHUB_SHA contains id of latest commit in default branch
+# 3. valid token to authenticate to Github REST API
+# 4. which repo to create release in as specfied by repo owner and name
+release_url=$(dotnet gitreleasemanager create \
+--milestone $milestone_name \
+--targetcommitish $GITHUB_SHA \
+--token $repo_token \
+--owner $owner \
+--repository $repository)
+
+# If anything went wrong with gitreleasemanager, use `error` logging command to output an error message,
+# and tell workflow that this action has failed by exiting with a non-zero return code.
+if [ $? -ne 0 ]; then
+  echo "::error::Failed to create the release draft"
+  exit 1
+fi
+
+# logging command for github action to set an output parameter - use value of release-url constructed above
+echo "::set-output name=release-url::$release_url"
 
 # tell workflow that action completed successfully by setting a 0 exit code
 exit 0
